ff9acccd07cc5deaff2020db06478163cb3b7e70
Sean Colyer <sean@colyer.name>
2012-07-05 21:41:40 -0400

    Updating manifest to version 2.

---
+++ src/gmail-cryptBackground.js
@@ -0,0 +1,59 @@
+/* This is the background page for gmail-crypt that communicates between gmail and the extension. 
+ * 
+ * Copyright 2011 Sean Colyer, <sean @ colyer . name>
+ * This program is licensed under the GNU General Public License Version 2. 
+ * See included "LICENSE" file for details.
+ */
+
+//TODO how do we want to handle errors?
+chrome.extension.onRequest.addListener(function(request,sender,sendResponse){
+    openpgp.keyring.init(); //We need to handle changes that might have been made.
+    if(request.method == "getAllPublicKeys"){
+        sendResponse(openpgp.keyring.publicKeys);
+    }
+    if(request.method == "getPublicKeys"){
+        var keys = {};
+        for(var email in request.emails){
+            try{
+            if(request.emails[email].length>0){
+                keys[request.emails[email]] = openpgp.keyring.getPublicKeyForAddress(request.emails[email])[0].armored;
+                }
+            }
+            catch(e){
+            
+            }
+        }
+        sendResponse(keys);
+    }
+    if(request.method == "getPublicKey"){
+        sendResponse(openpgp.keyring.getPublicKeyForAddress(request.email));
+    }
+    if(request.method == "getPrivateKey"){
+        sendResponse(openpgp.keyring.getPrivateKeyForAddress(request.email));
+    }
+    if(request.method == "getPrivateKeys"){
+        sendResponse(openpgp.keyring.privateKeys);
+    }
+    if(request.method == "getOption"){
+        openpgp.config.read();
+        var gCryptSettings = openpgp.config.config.gCrypt;
+        if(!gCryptSettings){
+            sendResponse('');
+        }
+        else{
+            sendResponse(gCryptSettings[request.option]);
+        }
+    }
+    if(request.method == "getConfig"){
+        sendResponse(openpgp.config);
+    }
+    else{
+    }
+});
+
+
+function onLoad(){
+    openpgp.init();
+}
+
+document.onload = onLoad();
